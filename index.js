e8f19d11a118d24cd6a0a4430db42ea8fd842fdd
w3sl1y <SV73623892@idat.edu.pe>
2021-08-30 21:35:01 -0500

    Actualización

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let { name, email } = { name: "John", email: "john@example.com" }
-console.log(name) // "John" console.log(email) // "john@example.com"
+let info = { 
+    name: "Wesley",
+    email: "wesleysneyd@hotmail.com", 
+    city: "Lima", 
+    state: "SF", 
+    country: "Perú",
+};
+
+let rest = {city: "Lima", state: "SF", country: "Perú"
+};
+
+let {name, email, city, state, country} = info;
+
+console.log(info)
